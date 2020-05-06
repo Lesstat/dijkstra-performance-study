f132eef980d8243a00705204b1e483358eda4190
Florian Barth <florianbarth@gmx.de>
2020-05-06 12:12:46 +0200

    Implement Dijkstra as in Wikipedia
    https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode

--- src/dijkstra.rs
+++ src/dijkstra.rs
@@ -1,6 +1,8 @@
 use crate::graph::Graph;
 use crate::NodeId;
 
+use std::collections::BTreeSet;
+
 pub struct Dijkstra<'a> {
     g: &'a Graph,
 }
@@ -11,6 +13,37 @@ impl<'a> Dijkstra<'a> {
     }
 
     pub fn dist(&mut self, from: NodeId, to: NodeId) -> u32 {
-        0
+        let mut dist = vec![u32::MAX; self.g.node_count()];
+        let mut prev: Vec<Option<NodeId>> = vec![None; self.g.node_count()];
+        let mut q: BTreeSet<_> = (0..self.g.node_count()).map(NodeId).collect();
+
+        dist[from.0] = 0;
+
+        while !q.is_empty() {
+            let u = Self::minimum_vertex(&q, &dist);
+            q.remove(&u);
+
+            for edge in self.g.outgoing_edges_of(u) {
+                let alt = dist[u.0] + edge.dist;
+                if alt < dist[edge.to.0] {
+                    dist[edge.to.0] = alt;
+                    prev[edge.to.0] = Some(u);
+                }
+            }
+        }
+
+        dist[*to]
+    }
+
+    fn minimum_vertex(q: &BTreeSet<NodeId>, dist: &Vec<u32>) -> NodeId {
+        let mut min_node = (NodeId(usize::MAX), u32::MAX);
+
+        for v in q {
+            if dist[v.0] < min_node.1 {
+                min_node = (*v, dist[v.0]);
+            }
+        }
+
+        min_node.0
     }
 }
